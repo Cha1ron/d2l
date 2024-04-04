e518c1e80089a6aec8aa3716fa9694e52ea2eb9e
Cha1ron <a992859805@126.com>
2024-04-04 10:54:35 +0800

    4.2

--- Chapter 4/4.2 MLP/run.py
+++ Chapter 4/4.2 MLP/run.py
@@ -1,5 +1,10 @@
 import torch
 from torch import nn
+import torchvision
+from torchvision import transforms
 
 batch_size = 256
-train_iter, test_iter = 
+trans = transforms.ToTensor()
+mnist_train = torchvision.datasets.FashionMNIST(root='../data/MNIST', train=True, transform=trans, download=True)
+dataloader_workers = 4
+train_iter, test_iter = torch.utils.data.DataLoader(mnist_train, batch_size, shuffle=True, num_workers=4), torch.utils.data.DataLoader(mnist_train, batch_size, shuffle=False, num_workers=4)
